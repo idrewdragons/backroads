9a679584b5783102b821e588270b99643246f7a2
Andrew Carey <andrewcarey1111@gmail.com>
2023-08-16 17:58:35 -0400

    first Commit

---
+++ src/components/Hero.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      {" "}
+      <section className="hero" id="home">
+        <div className="hero-banner">
+          <h1>Back Roads App</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+            explicabo debitis est autem dicta.
+          </p>
+          <a href="#tours" className="btn hero-btn">
+            explore tours
+          </a>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Hero;
